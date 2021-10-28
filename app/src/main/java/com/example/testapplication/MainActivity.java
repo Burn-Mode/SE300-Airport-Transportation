344672d30fa8114bcaecb34707814f00f6b2c471
Nicolas <nicolas.rod247@gmail.com>
2021-10-28 15:28:41 -0400

    Rideshare -> uber

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -6,16 +6,27 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openRideShare();
+            }
+        });
     }
-    public void browser1(View view){
-    Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.uber.com/"));
-    startActivity(browserIntent);
+
+    public void openRideShare() {
+        Intent intent = new Intent(this, RideShare.class);
+        startActivity(intent);
     }
+
 }
